f3466040b4c01680e2828f7ab75bafe2294b1b54
yutaka <shigure@refy.net>
2016-10-16 09:49:34 +0900

    mstcc service works correctly on device booted

--- app/src/main/java/net/refy/android/mstcc/receivers/BootReceiver.java
+++ app/src/main/java/net/refy/android/mstcc/receivers/BootReceiver.java
@@ -4,12 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import net.refy.android.mstcc.services.MsTccService;
+
 /**
  * Created by yutaka on 10/15/2016.
  */
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Intent i = new Intent(context.getApplicationContext(), MsTccService.class);
+        context.getApplicationContext().startService(i);
     }
 }
